d1f5b5f99cfddc7c2c239fc93cab083b259f73fb
Naman Bansal <59681930+Bansalnaman5@users.noreply.github.com>
2022-05-22 11:23:12 +0530

    Update messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -4,5 +4,7 @@ const controler=require('../controllers/messages');
 
 router.post('/message',controler.saveText);
 router.get('/message/list',controler.getText);
+router.get('/getUser',controler.getUser);
+router.post('/updateUser',controler.updateUser);
 
 module.exports=router;
